366b45bb9615bcb16ac64f940b9814426c67bf6f
LoHenP <qwpoal119@naver.com>
2023-12-07 14:56:19 +0900

    다시다시

--- board/entity/Board.cpp
+++ board/entity/Board.cpp
@@ -25,7 +25,7 @@ Board::Board(int id, const std::string& title, const std::string& writer, const 
           writer(writer),
           content(content) {}
 
-Board::Board(int id, const std::string& title, int writer, const std::string& content,
+Board::Board(int id, const std::string& title, const std::string& writer, const std::string& content,
       const std::string& reg_date, const std::string& upd_date)
         : id(id),
           title(title),
